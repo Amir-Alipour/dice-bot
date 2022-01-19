d31abdb8185f9c603842fe1e754ba74b86409c0e
amir alipour <amir.alipour12127@gmail.com>
2022-01-19 18:33:54 +0330

    ready for start

--- bot.js
+++ bot.js
@@ -1,6 +1,12 @@
+require("dotenv").config();
+
 const { Client, Intents } = require('discord.js');
 const client = new Client({intents: [
     Intents.FLAGS.GUILDS,
     Intents.FLAGS.GUILD_MESSAGES
 ]})
+
+client.login(process.env.TOKEN);
+
+
 
